3e59aec06ecaaea1d32127b984deb565dd4f452a
Devin Ghumman <ghummandevin@gmail.com>
2019-06-23 15:12:24 -0700

    Added Stuff b4 github

--- limbo/test.cpp
+++ limbo/test.cpp
@@ -57,56 +57,52 @@ int main(){
            glfwWindowShouldClose(window) == 0 ) {
 
     
-    
-    for (int i=0; i<unsortedTriangles.size(); i++) {
-        glm::vec3 camPos  = cameraManager.getCameraPosition();
-        float dist = -unsortedTriangles[i].fakedist2(Point(camPos.x, camPos.y, camPos.z));
-        triangleSortData[i] = (make_pair(dist, i));
-    }
-    std::sort(triangleSortData.begin(), triangleSortData.end());
-    for (int i=0; i<triangleSortData.size(); i++) {
-        sortedTriangles[i] = unsortedTriangles[triangleSortData[i].second];
-    }
-    g_vertex_buffer.resize(0);
-    ShapeBuilder::convertToPoints(sortedTriangles, &g_vertex_buffer);
-    
-    // This will identify our vertex buffer
-    GLuint vertexbuffer;
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
-    glBufferData(GL_ARRAY_BUFFER, g_vertex_buffer.size()*sizeof(g_vertex_buffer.data()), g_vertex_buffer.data(), GL_STATIC_DRAW);
-    
-    // One color for each vertex. They were generated randomly.
-    static vector<GLfloat> g_color_buffer;
-    for (int i=0; i<g_vertex_buffer.size(); i++) {
-        g_color_buffer.push_back(float_rand( 0.0f, 1.0f));
-    }
-    
-    GLuint colorbuffer;
-    glGenBuffers(1, &colorbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(GL_ARRAY_BUFFER, g_color_buffer.size()*sizeof(g_color_buffer.data()), g_color_buffer.data(), GL_STATIC_DRAW);
-    
-    // 2nd attribute buffer : colors
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glVertexAttribPointer(
-      1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-      3,                                // size
-      GL_FLOAT,                         // type
-      GL_FALSE,                         // normalized?
-      0,                                // stride
-      (void*)0                          // array buffer offset
-    );
+        
+        for (int i=0; i<unsortedTriangles.size(); i++) {
+            glm::vec3 camPos  = cameraManager.getCameraPosition();
+            float dist = -unsortedTriangles[i].fakedist2(Point(camPos.x, camPos.y, camPos.z));
+            triangleSortData[i] = (make_pair(dist, i));
+        }
+        std::sort(triangleSortData.begin(), triangleSortData.end());
+        for (int i=0; i<triangleSortData.size(); i++) {
+            sortedTriangles[i] = unsortedTriangles[triangleSortData[i].second];
+        }
+        g_vertex_buffer.resize(0);
+        ShapeBuilder::convertToPoints(sortedTriangles, &g_vertex_buffer);
+        
+        // This will identify our vertex buffer
+        GLuint vertexbuffer;
+        // Generate 1 buffer, put the resulting identifier in vertexbuffer
+        glGenBuffers(1, &vertexbuffer);
+        // The following commands will talk about our 'vertexbuffer' buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        // Give our vertices to OpenGL.
+        glBufferData(GL_ARRAY_BUFFER, g_vertex_buffer.size()*sizeof(g_vertex_buffer.data()), g_vertex_buffer.data(), GL_STATIC_DRAW);
+        
+        // One color for each vertex. They were generated randomly.
+        static vector<GLfloat> g_color_buffer;
+        for (int i=0; i<g_vertex_buffer.size(); i++) {
+            g_color_buffer.push_back(float_rand( 0.0f, 1.0f));
+        }
+        
+        GLuint colorbuffer;
+        glGenBuffers(1, &colorbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glBufferData(GL_ARRAY_BUFFER, g_color_buffer.size()*sizeof(g_color_buffer.data()), g_color_buffer.data(), GL_STATIC_DRAW);
+        
+        // 2nd attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+          1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+          3,                                // size
+          GL_FLOAT,                         // type
+          GL_FALSE,                         // normalized?
+          0,                                // stride
+          (void*)0                          // array buffer offset
+        );
 
     
-//    // Check if the ESC key was pressed or the window was closed
-//    while (glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-//           glfwWindowShouldClose(window) == 0 ) {
-
         glm::mat4 mvp;
         {
             double x,y;
